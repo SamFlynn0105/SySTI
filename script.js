143ec4d0a0064b7522d116b6ae72b12d0d74657d
SamFlynn0105 <storrijos1100@outlook.com>
2024-05-14 13:40:27 -0600

    cambios a sitio

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ buttonMenu.addEventListener('click', (e) => {
   e.currentTarget.classList.toggle('nav-open');
   navMenu.classList.toggle('open-menu');
 });
+
+navMenu.addEventListener('click', (e) => {
+  e.currentTarget.classList.remove('open-menu');
+  buttonMenu.classList.remove('nav-open');
+});
