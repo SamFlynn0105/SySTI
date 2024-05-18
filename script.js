ec7a4d9cbc84024d130318b4bd7809b00b7606aa
SamFlynn0105 <storrijos1100@outlook.com>
2024-05-18 17:40:48 -0600

    se agregan funciones a la pagina

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let slideIndex = 0;
+
+  //console.log(widthtam1.matches)
+showSlides();
+
 // JavaScript Version
 const buttonMenu = document.querySelector('#nav-mobile');
 const navMenu = document.querySelector('.nav-menu');
@@ -11,3 +16,34 @@ navMenu.addEventListener('click', (e) => {
   e.currentTarget.classList.remove('open-menu');
   buttonMenu.classList.remove('nav-open');
 });
+
+
+function showSlides() {
+  let i;
+  
+  //widthtam1 = window.matchMedia("(max-width: 700px)");
+  const slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  
+  let widthtam1 = window.matchMedia("(max-width: 700px)");
+    console.log(widthtam1.matches)
+  
+  if (widthtam1.matches) 
+      {
+        slides[slideIndex-1].style.display = "grid";
+      }
+      else 
+      {
+        slides[slideIndex-1].style.display = "block";   
+      }    
+  setTimeout(showSlides, 9000); // Cambiar cada 2 segundos (2000 milisegundos)
+}
+
+// Función para avanzar o retroceder las diapositivas
+/*function plusSlides() {
+  showSlides();
+}*/
